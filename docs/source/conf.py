723857b5f7eb1e28ffd9b569d8b38c3539a53b41
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2020-01-21 18:49:22 -0700

    Bump copyright year on docs
    Adding the LZ10 CLI was a significant enough change that this is reasonable now, I believe

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ndspy'
-copyright = '2019, RoadrunnerWMC'
+copyright = '2020, RoadrunnerWMC'
 author = 'RoadrunnerWMC'
 
 # The version info for the project you're documenting, acts as replacement for
