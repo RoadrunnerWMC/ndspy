a6aa70c38e51af4b170cb151acbc595f1aa3158b
Mike <mikebuntu68@gmail.com>
2023-10-20 15:49:24 -0400

    Add `py.typed` file

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/RoadrunnerWMC/ndspy',
     packages=setuptools.find_packages(),
+    package_data={
+        'ndspy': ['py.typed'],
+    },
     python_requires='>=3.7',
     entry_points={
         'console_scripts': [
